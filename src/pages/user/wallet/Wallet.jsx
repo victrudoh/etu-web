9c9cbd69b0a16f438f19042e7e9dc4faae3af66e
Edikan <victrudoh@gmail.com>
2022-08-07 00:41:39 +0100

    just changed a few icons

--- src/pages/user/wallet/Wallet.jsx
+++ src/pages/user/wallet/Wallet.jsx
@@ -11,18 +11,18 @@ const Wallet = () => {
         <Content>
           <Left>
             <NavLink to="/user/wallet/create">
-              <i className="bx bx-wallet"></i> Create Wallet
+              <i className="bx bx-plus-circle"></i> Create Wallet
             </NavLink>
             <NavLink to="/user/wallet/deposit">
-              <i className="bx bx-wallet"></i> Make Deposit
+              <i className="bx bx-chevrons-down"></i>Make Deposit
             </NavLink>
           </Left>
           <Left>
             <NavLink to="/user/wallet/transfer">
-              <i className="bx bx-wallet"></i> Transfer Funds
+              <i className="bx bx-transfer"></i> Transfer Funds
             </NavLink>
             <NavLink to="/user/wallet/withdraw">
-              <i className="bx bx-wallet"></i> Withdraw Funds
+              <i className="bx bx-chevrons-up"></i>Withdraw Funds
             </NavLink>
           </Left>
         </Content>
